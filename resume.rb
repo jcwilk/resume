662459e39f02f71b28555917daf9161b49986beb
John Wilkinson <jcwilk@gmail.com>
2012-04-14 21:19:28 -0700

    Add more sections and a bit more styling

--- resume.rb
+++ resume.rb
@@ -2,6 +2,17 @@ class Resume < Sinatra::Base
   before do
     @title = nil
     @name, @street, @city, @state, @email, @github_link = nil
+    @misc_sections = {
+      'Goals' => 'Productivity!'
+    }
+    @jobs = [
+      #position, date string, company identification, description
+      ['Software Engineer','April 2006 - April 2007','Scumsoft, Yosemite, CA','Wrote jello capture software.']
+    ]
+    @schools = [
+      #degree, date string, school identification
+      ['Computer Science','June 2006','Beer Academy']
+    ]
   end
 
   get "/" do
