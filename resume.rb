9b494b8496825201d6a16d31a86dfab617666cee
John Wilkinson <jcwilk@gmail.com>
2012-04-14 23:26:30 -0700

    Add more personal info

--- resume.rb
+++ resume.rb
@@ -1,7 +1,7 @@
 class Resume < Sinatra::Base
   before do
     @title = nil
-    @name, @street, @city, @state, @email, @github_link = ['John Wilkinson','Berkeley, CA','jcwilk@email.com']
+    @name, @street, @city, @state, @email, @github_link = ['John Wilkinson','Berkeley, CA','jcwilk at gmail.com','http://github.com/jcwilk']
     @street = nil #it'll hide the line if you omit
 
     @misc_sections = {
@@ -10,14 +10,26 @@ class Resume < Sinatra::Base
     }
     @jobs = [
       #position, date string, company identification, description
-      ['Software Engineer','April 2006 - April 2007','Scumsoft, Yosemite, CA','Wrote jello capture software.']
+      [
+        'Rails Engineer with API/Services focus',
+        'Feb 2010 to March 2012',
+        'Howcast Media, San Francisco, CA',
+        'Helped work towards rebuilding an oversized 2.3 app into satellite SOA parts running on Engine Yard Cloud. Broke users code/db into its own dedicated SSO and API service. Worked with management to architect next generation of SOA apps to replace the legacy app. Replaced in-house transcoding system with 3rd party integration to Zencoder while maintaining legacy formats.'
+      ],
+      [
+        'Rails Engineer',
+        'Summer 2007 to Fall 2009',
+        'CrystalCommerce, Seattle, WA',
+        'Worked with one to two other developers and the CEO on a rails 2.x app which provided a platform storefront and backend designed for inventory maintenance of tens of thousands of closely related collectibles across dozens of clients and domainnames. Integrated ActiveMerchant into a new checkout system. Designed a simple web-based POS to reuse as much behavior from the checkout system as possible.'
+      ]
     ]
     @schools = [
       #degree, date string, school identification
-      ['Computer Science','June 2006','Beer Academy']
+      ['BS in Computer Science','June 2008','Seattle University, Seattle, WA']
     ]
     @footer_links = [
-      ['http://www.google.com/','Find me here! Somehow...']
+      ['http://jcwilk-resume.heroku.com/','Heroku hosted'],
+      ['http://github.com/jcwilk/resume','Github coded']
     ]
   end
 
