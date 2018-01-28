c23b56fe248ac7838d5579660a4bfc6a43719d55
John Wilkinson <jcwilk@gmail.com>
2018-01-28 21:06:00 +0800

    Update resume content to be current

--- lib/resume.rb
+++ lib/resume.rb
@@ -13,21 +13,21 @@ class Resume
   def initialize
     @title = 'John Wilkinson'
     @name, @city, @state, @email, @github_link = ['John Wilkinson','Singapore','Singapore','jcwilk at gmail.com','http://github.com/jcwilk']
-    @street = '2 Leedon Road' #it'll hide the line if you omit
+    @street = 'Bukit Batok East' #it'll hide the line if you omit
   end
 
   def misc_sections
     {
       'Objective' => [
-        'To design, build, refactor, and extend innovative web projects and experiment with new technologies as a part of a motivated, respectful, and cooperative team with freedom to hone unique skills to be shared between ourselves. Long sentences are a plus.'
+        'To design, build, refactor, and extend innovative projects and experiment with new technologies as a part of a motivated, respectful, and cooperative team with freedom to hone unique skills to be shared between ourselves. Long sentences are a plus.'
       ],
       'Skills' => [
-        'Well versed in Ruby and JavaScript, adequate in Python, C++, BASH, and Java, and enthused and fearless about diving into any other language',
-        'Passionate about seeking the perfect balance of testing across the various possible layers to drive the given situation',
-        'Architected a variety of RESTful APIs and experienced first hand the value tradeoffs in deciding between designing for maintainability or organic growth',
-        'Comforable diving into a database CLI or debugging slow/eroneous queries through more indirect means',
-        'Eager to play devil\'s advocate and wrestle out key blockades along the way before too much code gets invested',
-        'Works refactoring in as an inherent part of moving forward with development to keep technical debt at the level appropriate for the project'
+        'Well versed in Ruby, JavaScript, and C#, adequate in Python, C++, BASH, and Java, and enthused and fearless about diving into any other language.',
+        'Passionate about seeking the perfect balance of testing across the various possible layers to drive the given situation.',
+        'Architected a variety of RESTful APIs and experienced first hand the value tradeoffs in deciding between designing for maintainability or organic growth.',
+        'Comforable diving into a database CLI or debugging slow/eroneous queries through more indirect means.',
+        'Eager to play devil\'s advocate and wrestle out key blockades along the way before too much code gets invested.',
+        'Works refactoring in as an inherent part of moving forward with development to keep technical debt at the level appropriate for the project.'
       ],
       'Preferred Development Style' => [
         'git with something like <a href="http://scottchacon.com/2011/08/31/github-flow.html">Github Workflow</a> to keep master safe and valuable',
@@ -48,6 +48,43 @@ class Resume
   def jobs
     [
       #position, date string, company identification, description
+      [
+        'Games Engineer',
+        'August 2017 to Present',
+        'Springloaded, Singapore',
+        [
+          'Ported and released two games for the Nintendo Switch from legacy mobile code: "Hiragana Pixel Party" and "Nuclien".',
+          'Helped finish up another Switch game, "GORSD", still unreleased as we search for publishers.',
+          'Leading the development of the battle animation system for "Tiny Dice Dungeon 2", the successor to "Tiny Dice Dungeon", still in development.'
+        ]
+      ],
+      [
+        'CTO',
+        'May 2016 to Present',
+        'Gimmie, Singapore',
+        [
+          'Managed all technical aspects of the company.',
+          'Worked with the business stakeholder to find solutions that fit our tech structure.',
+          'Wrote new version of the Rails server application mostly from scratch based on learned success from previous code.',
+          'Handled setup, deployment, and maintenance of AWS server stack.',
+          'Architected event pipeline built on top of Segment and Keen for tracking user events.',
+          'Hired and managed a remote engineer.',
+          'Built solutions to integrate with legacy 3rd party systems.'
+        ]
+      ],
+      [
+        'Principal Engineer',
+        'May 2015 to May 2016',
+        'ReferralCandy, Singapore',
+        [
+          'Led the team of Rails engineers through process and infrastructure improvements.',
+          'Got the team onto Github with a pull request driven development structure.',
+          'Converted shared libraries into versioned internal gems.',
+          'Pushed for more emphasis on code quality and test driven code.',
+          'Led the hiring process.',
+          'Spent the majority of my time developing Rails server code.'
+        ]
+      ],
       [
         'Rails Software Engineer/Software Architect',
         'March 2012 to October 2014',
@@ -92,10 +129,42 @@ class Resume
     [
       #title, url, description
       [
-        'Being Awesome',
-        'http://www.google.com',
+        'Material Girl',
+        'https://www.lexaloffle.com/bbs/?pid=39675&tid=29166',
         [
-          'Self explanatory!'
+          'Small but complete game for the Pico-8 platform.',
+          'Mashup between JRPG, Dating Sim, and puzzle game.',
+          'Focused heavily on a promises-inspired pattern for handling sequential tweens.',
+          'Designed custom tween and sprite tracking mini libraries.',
+          'Used Ruby for compiling multiple files of code into the final cart for easier organization.'
+        ]
+      ],
+      [
+        'Anx',
+        'https://www.lexaloffle.com/bbs/?pid=42366&tid=29630',
+        [
+          'Still WIP first person 3D game for the Pico-8 platform.',
+          'Simulation of social anxiety as a game mechanic.',
+          'Built the raycasting 3D engine from scratch which runs in a strictly 2D platform.',
+          'Due to lack of 3D hardware support the rendering automatically degrades in quality to maintain framerate.'
+        ]
+      ],
+      [
+        'Divided',
+        'https://github.com/jcwilk/divided',
+        [
+          'Browser-based, room-based, turn-based, pixel art WIP MMO',
+          'Runs in eventmachine on the server using long-poll to deliver events.',
+          'Heavily API driven with a HAL hypermedia structure.',
+          'Due to the ambitious nature, development has been paused until I get through smaller projects.',
+          'Not currently hosted online but the prototype is functional.'
+        ]
+      ],
+      [
+        'Misc. Github Projects/Contributions',
+        'https://github.com/jcwilk',
+        [
+          'Quite a lot of smaller projects and forks with contributions to open source software.'
         ]
       ]
     ]
